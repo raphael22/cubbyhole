ee0c33fec6b3382d4efe6526584866fb3b798e78
raphael22 <rmlx22@gmail.com>
2014-03-07 18:10:35 +0100

    7/3/14 18:10

--- home.php
+++ home.php
@@ -2,5 +2,14 @@
 	<h2 class="title">Welcome on CubbyHole</h2>
 	<h4 class="btn btn-nav">Sign up for free</h4>
 	<h4 class="btn btn-nav" data-uri="plans">See our Plans</h4>
-
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
+	<img src="bse.jpg"/>
 </section>

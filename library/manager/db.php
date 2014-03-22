4f033df40ed4303f9c2fda8b783cfbd67e0928b0
raphael22 <rmlx22@gmail.com>
2014-03-22 20:27:36 +0100

    R2027 test mysql3

--- library/manager/db.php
+++ library/manager/db.php
@@ -6,8 +6,7 @@
 	    $username = $url["user"];
 	    $password = $url["pass"];
 	    $dbname = substr($url["path"],1);
-
-		$db = new PDO('mysql:'+$server+';dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
+		$db = new PDO('mysql:host='+$server+';dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
 	}
 	catch (Exception $e){
 		die('Erreur : ' . $e->getMessage());

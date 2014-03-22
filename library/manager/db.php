8056afbdfb1679ece3e75537a2d2512a3fc9ebf6
raphael22 <rmlx22@gmail.com>
2014-03-22 20:31:50 +0100

    R2031 test mysql4

--- library/manager/db.php
+++ library/manager/db.php
@@ -1,4 +1,7 @@
 <?php
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
 	try{
 		$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
@@ -6,9 +9,10 @@
 	    $username = $url["user"];
 	    $password = $url["pass"];
 	    $dbname = substr($url["path"],1);
-		$db = new PDO('mysql:host='+$server+';dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
+		$db = new PDO('mysql:host='.$server.';dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
 	}
 	catch (Exception $e){
 		die('Erreur : ' . $e->getMessage());
+		echo "<script>console.log(".$server.");</script>"
 	}
 ?>

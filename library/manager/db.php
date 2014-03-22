a568eadf721cdd2a3179815ff40a70030581ddd7
raphael22 <rmlx22@gmail.com>
2014-03-22 20:22:04 +0100

    R2021 test mysql

--- library/manager/db.php
+++ library/manager/db.php
@@ -1,11 +1,13 @@
 <?php
-
 	try{
-		$db = new PDO('pgsql:dbname=d2ni6f5uvtc435;												
-						user=igmqhmhbitdyon;
-						password=Uy8MJmzLjZ3iWTqgzGL0thYzbf;
-						host=ec2-50-19-219-235.compute-1.amazonaws.com;
-						port=5432');
+		$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	    $server = $url["host"];
+	    $username = $url["user"];
+	    $password = $url["pass"];
+	    $db = substr($url["path"],1);
+
+		$db = new PDO('mysql:'+$server+';dbname='+$db, $username, $password);
 	}
 	catch (Exception $e){
 		die('Erreur : ' . $e->getMessage());

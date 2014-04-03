82f749cfae917d666291d96d72027be17b815f37
raphael22 <rmlx22@gmail.com>
2014-04-03 13:57:27 +0200

    R1357 test host

--- library/manager/db.php
+++ library/manager/db.php
@@ -4,7 +4,7 @@ if($local==false){
 	try{
 		$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 	    $server = $url["host"];
-		$db = new PDO('mysql:host='.$server.';dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
+		$db = new PDO('mysql:host=mysql://b50637b0a0e28d:9ce391b5@us-cdbr-east-05.cleardb.net;dbname=heroku_7d0cbf23e0e57a1', 'b50637b0a0e28d', '9ce391b5');
 	}
 	catch (Exception $e){
 		die("Error : " . $e->getMessage());

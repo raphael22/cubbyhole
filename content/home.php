0b3a45f3fcd425713d444c854d032ac8657cc48e
raphael22 <rmlx22@gmail.com>
2014-06-05 17:28:51 +0200

    Last Update

--- content/home.php
+++ content/home.php
@@ -10,7 +10,7 @@ if(isset($_SESSION["userRole"])) {
 		
 	<h2><?php 
 		echo Welcome.' ';
-		$userManager->getUserName($_SESSION["userId"],$db); 
+		echo $userManager->getUserName($_SESSION["userId"],$db); 
 	?></h2>
 	<section>
 		<h2>Update 0.1</h2>
